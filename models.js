160a9812e47fd4d05ffbe1c88642f7c5f927e642
Mike <mdwaud@gmail.com>
2018-10-18 13:51:13 -0400

    add fixture and implementation for question 1

--- models.js
+++ models.js
@@ -1,9 +1,18 @@
 module.exports = {
-  db_test: function(db, value) {
-    let sql = `SELECT ? as 'result';`;
-    const stmt = db.prepare(sql);
-    const result = stmt.get(value);
+  question_1: function(db) {
+    let sql = `
+    SELECT AGENT_NAME, DEPARTMENT_NAME, SUM(AMOUNT) AS \`TOTAL_SALES\`
+    FROM SALES_RECORDS
+    LEFT JOIN SALES_AGENTS USING(AGENT_ID)
+    LEFT JOIN DEPARTMENTS USING(DEPARTMENT_ID)
+    GROUP BY AGENT_ID
+    ORDER BY TOTAL_SALES DESC
+    ;
+    `;
 
-    return result['result']
+    const stmt = db.prepare(sql);
+    const result = stmt.all();
+
+    return result
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var assert = require('assert');
+const fs = require('fs')
 var models = require('../models.js')
 
+const sales_fixture = 'sales.sql'
 const Database = require('better-sqlite3');
 
 
@@ -15,10 +17,28 @@ describe('models', function() {
     db.close();
   })
 
-  describe('#db_test()', function() {
-    it('should add numbers', function() {
-      actual = models.db_test(db, 5)
-      assert.equal(actual, 5);
+  describe('#question_1()', function() {
+    before(function() {
+      let data = fs.readFileSync(sales_fixture, 'utf-8')
+      db.exec(data)
+    })
+
+    it('run the query', function() {
+      results = models.question_1(db)
+
+      // we should have 4 rows
+      assert.equal(results.length, 4);
+
+      // check the actual results
+      assert.equal(results[0]["AGENT_NAME"], "Frank Jones");
+      assert.equal(results[0]["DEPARTMENT_NAME"], "North America");
+      assert.equal(results[0]["TOTAL_SALES"], 2500);
+
+      assert.equal(results[1]["AGENT_NAME"], "Sally Smith");
+      assert.equal(results[1]["DEPARTMENT_NAME"], "North America");
+      assert.equal(results[1]["TOTAL_SALES"], 2200);
+
+      // etc...
     });
   });
 });
